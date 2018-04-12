d28394bdd11825f5c30fdac94e403078ba5fda6a
cristian.espinoza <cristian.espinoza@genneo.com.bo>
2018-04-11 21:59:23 -0400

    Actualización de la rutas de la aplicacion.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
 
+// Servicios
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-
 
 @NgModule({
   declarations: [
@@ -18,7 +22,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
