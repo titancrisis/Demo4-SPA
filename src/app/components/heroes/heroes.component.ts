a030f3cd0ad075cec3ca5083e8883b77f801e8ad
cristian.espinoza <cristian.espinoza@genneo.com.bo>
2018-04-12 20:41:13 -0400

    Creación del diseño de heroes con la lista de datos e imagenes.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService) {
+    console.log('constructor');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
+
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+
   }
 
 }
